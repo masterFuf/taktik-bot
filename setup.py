c23188e7f7a9af3caf321e3c752a42ec3bbe710a
KGalaffu <galaffu.dev@gmail.com>
2025-10-28 17:40:01 +0100

    chore:: bump version from 1.0.3 to 1.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="taktik-bot",
-    version="1.0.3",
+    version="1.0.4",
     packages=find_packages(),
     include_package_data=True,
     python_requires=">=3.10",
