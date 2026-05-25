4273afccf176c7f322677418e7703c1a37d283c4
masterFuf <galaffu.dev@gmail.com>
2026-05-25 02:36:09 +0200

    feat(story_selectors): update selectors for home feed stories and profile highlights

--- taktik/core/social_media/instagram/ui/selectors/story.py
+++ taktik/core/social_media/instagram/ui/selectors/story.py
@@ -10,12 +10,18 @@ class StorySelectors:
     story_image: str = '//android.widget.ImageView[contains(@resource-id, "reel_media_image")]'
     story_video: str = '//android.widget.VideoView[contains(@resource-id, "reel_media_video")]'
 
+    # === Home feed story tray ===
+    feed_story_tray: str = '//*[contains(@resource-id, "reels_tray_container")]'
+    feed_story_recycler: str = '//*[contains(@content-desc, "conteneur barre des reels") or contains(@content-desc, "reels tray")]'
+    feed_story_buttons: str = '//*[contains(@resource-id, "reels_tray_container")]//android.widget.Button[contains(translate(@content-desc, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"), "story")]'
+    feed_unseen_story_buttons: str = '//*[contains(@resource-id, "reels_tray_container")]//android.widget.Button[contains(translate(@content-desc, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"), "story") and (contains(translate(@content-desc, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"), "non vus") or contains(translate(@content-desc, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"), "unseen"))]'
+
     # === Profile / highlights ===
     profile_unseen_story_avatar: str = '//*[contains(translate(@content-desc, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"), "unseen story")]'
-    highlight_tray: str = '//*[@resource-id="com.instagram.android:id/highlights_tray"]'
-    highlight_recycler: str = '//*[@resource-id="com.instagram.android:id/highlights_reel_tray_recycler_view"]'
-    highlight_buttons: str = '//*[@resource-id="com.instagram.android:id/highlights_reel_tray_recycler_view"]//android.widget.Button[contains(translate(@content-desc, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"), "story")]'
-    highlight_images: str = '//*[@resource-id="com.instagram.android:id/highlights_reel_tray_recycler_view"]//*[contains(translate(@content-desc, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"), "highlight story")]'
+    highlight_tray: str = '//*[contains(@resource-id, "highlights_tray")]'
+    highlight_recycler: str = '//*[contains(@resource-id, "highlights_reel_tray_recycler_view")]'
+    highlight_buttons: str = '//*[contains(@resource-id, "highlights_reel_tray_recycler_view")]//android.widget.Button[contains(translate(@content-desc, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"), "story")]'
+    highlight_images: str = '//*[contains(@resource-id, "highlights_reel_tray_recycler_view")]//*[contains(translate(@content-desc, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"), "highlight story")]'
     
     # === Navigation ===
     next_story: str = '//android.widget.FrameLayout[contains(@resource-id, "story_viewer_container")]//android.widget.ImageView[contains(@content-desc, "Suivant") or contains(@content-desc, "Next")]'
@@ -23,20 +29,22 @@ class StorySelectors:
     
     # === Détection du nombre de stories ===
     # Viewer de story - contient "story X of Y" dans le content-desc
-    story_viewer_text_container: str = '//*[@resource-id="com.instagram.android:id/reel_viewer_text_container"]'
+    story_viewer_text_container: str = '//*[contains(@resource-id, "reel_viewer_text_container")]'
     
     # Header de story avec username et timestamp
-    story_viewer_header: str = '//*[@resource-id="com.instagram.android:id/reel_viewer_header"]'
-    story_viewer_title: str = '//*[@resource-id="com.instagram.android:id/reel_viewer_title"]'
-    story_viewer_timestamp: str = '//*[@resource-id="com.instagram.android:id/reel_viewer_timestamp"]'
+    story_viewer_header: str = '//*[contains(@resource-id, "reel_viewer_header")]'
+    story_viewer_title: str = '//*[contains(@resource-id, "reel_viewer_title")]'
+    story_viewer_timestamp: str = '//*[contains(@resource-id, "reel_viewer_timestamp")]'
     
     # Barre de progression des stories
-    story_progress_bar: str = '//*[@resource-id="com.instagram.android:id/reel_viewer_progress_bar"]'
-    story_viewer_root: str = '//*[@resource-id="com.instagram.android:id/reel_viewer_root"]'
+    story_progress_bar: str = '//*[contains(@resource-id, "reel_viewer_progress_bar")]'
+    story_viewer_root: str = '//*[contains(@resource-id, "reel_viewer_root")]'
     
     # Actions sur story
-    story_like_button: str = '//*[@resource-id="com.instagram.android:id/toolbar_like_button"]'
-    story_share_button: str = '//*[@resource-id="com.instagram.android:id/toolbar_reshare_button"]'
-    story_message_composer: str = '//*[@resource-id="com.instagram.android:id/message_composer_container"]'
+    story_like_button: str = '//*[contains(@resource-id, "toolbar_like_button")]'
+    story_share_button: str = '//*[contains(@resource-id, "toolbar_reshare_button")]'
+    story_message_composer: str = '//*[contains(@resource-id, "message_composer_container") or contains(@resource-id, "reel_viewer_message_composer")]'
+    story_reaction_toolbar: str = '//*[contains(@resource-id, "reel_reaction_toolbar")]'
+    story_reaction_emojis: str = '//*[contains(@resource-id, "story_reactions_emoji")]'
 
 STORY_SELECTORS = StorySelectors()
