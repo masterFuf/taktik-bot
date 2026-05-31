cfcfe2955d04ea65e159636794fd691bd9fde9b5
masterFuf <galaffu.dev@gmail.com>
2026-05-31 21:40:23 +0200

    Clean email package facade

--- taktik/core/app/email/__init__.py
+++ taktik/core/app/email/__init__.py
@@ -3,8 +3,3 @@
 from taktik.core.app.email.gmail.workflows.account import GmailWorkflow
 
 __all__ = ["GmailWorkflow"]
-"""Application email integrations."""
-
-from taktik.core.app.email.gmail.workflows.account import GmailWorkflow
-
-__all__ = ["GmailWorkflow"]
